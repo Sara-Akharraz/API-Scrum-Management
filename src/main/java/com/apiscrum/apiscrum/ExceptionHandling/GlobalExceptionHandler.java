92d0fff60402d87177502d0ee21d2eb9c979f1c0
Sara-Akharraz <saraakharraz22@gmail.com>
2025-04-04 18:57:56 +0000

    GlobalExceptionHandler Entity updated

--- src/main/java/com/apiscrum/apiscrum/ExceptionHandling/GlobalExceptionHandler.java
+++ src/main/java/com/apiscrum/apiscrum/ExceptionHandling/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package com.apiscrum.APIScrum.ExceptionHandling;
+package com.apiscrum.apiscrum.ExceptionHandling;
 
 import jakarta.persistence.EntityNotFoundException;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
