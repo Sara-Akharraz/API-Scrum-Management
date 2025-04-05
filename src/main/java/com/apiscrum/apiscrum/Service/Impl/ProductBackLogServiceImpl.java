ba5772c62306083f3b8547aa4c479be340ebaa5f
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2025-04-05 22:47:58 +0000

    Implement Product BackLog Service

--- src/main/java/com/apiscrum/apiscrum/Service/Impl/ProductBackLogServiceImpl.java
+++ src/main/java/com/apiscrum/apiscrum/Service/Impl/ProductBackLogServiceImpl.java
@@ -1,17 +1,25 @@
-package com.apiscrum.apiscrum.Service.Impl;
+package com.apiscrum.APIScrum.Service.Impl;
 
-import com.apiscrum.apiscrum.Entity.Epic;
-import com.apiscrum.apiscrum.Entity.ProductBackLog;
-import com.apiscrum.apiscrum.Entity.UserStory;
-import com.apiscrum.apiscrum.Repository.EpicRepository;
-import com.apiscrum.apiscrum.Repository.ProductBackLogRepository;
-import com.apiscrum.apiscrum.Repository.UserStoryRepository;
-import com.apiscrum.apiscrum.Service.ProductBackLogService;
+import com.apiscrum.APIScrum.Controller.ProductBackLogController;
+import com.apiscrum.APIScrum.DTO.ProductBackLogDto;
+import com.apiscrum.APIScrum.DTO.UserStoryDto;
+import com.apiscrum.APIScrum.Entity.*;
+import com.apiscrum.APIScrum.Entity.UserStory;
+import com.apiscrum.APIScrum.Mapper.ProductBackLogMapper;
+import com.apiscrum.APIScrum.Mapper.ProjectMapper;
+import com.apiscrum.APIScrum.Mapper.UserStoryMapper;
+import com.apiscrum.APIScrum.Repository.EpicRepository;
+import com.apiscrum.APIScrum.Repository.ProductBackLogRepository;
+import com.apiscrum.APIScrum.Repository.ProjectRepository;
+import com.apiscrum.APIScrum.Repository.UserStoryRepository;
+import com.apiscrum.APIScrum.Service.ProductBackLogService;
+import com.apiscrum.APIScrum.Service.UserStoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductBackLogServiceImpl implements ProductBackLogService {
@@ -22,73 +30,76 @@ public class ProductBackLogServiceImpl implements ProductBackLogService {
     EpicRepository epicRepository;
     @Autowired
     UserStoryRepository userStoryRepository;
+    @Autowired
+    ProjectRepository projectRepository;
     @Override
-    public void addProductBackLog(ProductBackLog productBackLog) {
-        productBackLogRepository.save(productBackLog);
+    public ProductBackLogDto addProductBackLog(ProductBackLogDto productBackLog, Long id_project) {
+        Project project = projectRepository.findById(id_project)
+                .orElseThrow(() -> new RuntimeException("Product Backlog not found with id: " + id_project));
+        productBackLog.setProject(project);
+        return ProductBackLogMapper.mapToProductBackLogDTO(productBackLogRepository.save(
+                ProductBackLogMapper.mapToProductBackLog(productBackLog)
+        ));
     }
 
     @Override
-    public void updateProductBackLog(Long id, ProductBackLog productBackLog) {
+    public ProductBackLogDto updateProductBackLog(Long id, ProductBackLogDto productBackLog) {
         ProductBackLog pb = productBackLogRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Product Backlog not found with id: " + id));
         pb.setProject(productBackLog.getProject());
         pb.setEpics(productBackLog.getEpics());
         pb.setTitle(productBackLog.getTitle());
         pb.setUserStories(productBackLog.getUserStories());
-        productBackLogRepository.save(pb);
+        ProductBackLog p = productBackLogRepository.save(pb);
+        System.out.println("-------------------------------------------"+ pb.getId());
+        return ProductBackLogMapper.mapToProductBackLogDTO(p);
     }
 
     @Override
     public void deleteProductBackLog(Long id) {
-        productBackLogRepository.deleteById(id);
+        if(productBackLogRepository.existsById(id))
+            productBackLogRepository.deleteById(id);
+        else
+            throw new RuntimeException("Product Backlog not found with id: " + id);
+
     }
 
     @Override
-    public Optional<ProductBackLog> getProductBackLog(Long id) {
-        return productBackLogRepository.findById(id);
+    public Optional<ProductBackLogDto> getProductBackLog(Long id) {
+
+        return productBackLogRepository.findById(id).map(ProductBackLogMapper::mapToProductBackLogDTO);
     }
 
     @Override
-    public List<ProductBackLog> getAllProductBackLog() {
-        return productBackLogRepository.findAll();
+    public List<ProductBackLogDto> getAllProductBackLog() {
+        List<ProductBackLog> pbs = productBackLogRepository.findAll();
+        return pbs.stream().map((pb) -> ProductBackLogMapper.mapToProductBackLogDTO(pb))
+                .collect(Collectors.toList());
     }
 
     @Override
-    public void addEpic(Long id, Epic epic) {
-        ProductBackLog productBackLog = productBackLogRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id));
-        productBackLog.getEpics().add(epic);
+    public ProductBackLogDto addUserStoryToPB(UserStoryDto userStory, Long id) {
+        ProductBackLog pb = productBackLogRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Product Backlog not found with id: " + id));
+        userStory.setProductBackLog(pb);
+        userStoryRepository.save(UserStoryMapper.mapToUserStory(userStory));
+        return ProductBackLogMapper.mapToProductBackLogDTO(pb);
     }
+//
+//    @Override
+//    public void addEpic(Long id, Epic epic) {
+//        ProductBackLog productBackLog = productBackLogRepository.findById(id)
+//                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id));
+//        productBackLog.getEpics().add(epic);
+//    }
+//
+//
+//
+//    @Override
+//    public void addUserStory(Long id_p, UserStory userStory) {
+//        ProductBackLog productBackLog = productBackLogRepository.findById(id_p)
+//                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id_p));
+//        productBackLog.getUserStories().add(userStory);
+//    }
 
-    @Override
-    public void deleteEpic(Long id_p, Long id) {
-        ProductBackLog productBackLog = productBackLogRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id));
-        Epic epic = epicRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Epic not found with id: " + id));
-        productBackLog.getEpics().remove(epic);
-    }
-
-    @Override
-    public void addUserStory(Long id_p, UserStory userStory) {
-        ProductBackLog productBackLog = productBackLogRepository.findById(id_p)
-                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id_p));
-        productBackLog.getUserStories().add(userStory);
-    }
-
-    @Override
-    public void deleteUserStory(Long id_p,Long id_us){
-        ProductBackLog productBackLog = productBackLogRepository.findById(id_p)
-                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id_p));
-        productBackLog.getUserStories().remove(id_us);
-    }
-
-    @Override
-    public void deleteUserStoryInBaclog(Long id_us, Long id_pb) {
-        ProductBackLog productBackLog = productBackLogRepository.findById(id_pb)
-                .orElseThrow(() -> new RuntimeException("ProductBackLog not found with id: " + id_pb));
-        UserStory us = userStoryRepository.findById(id_us)
-                        .orElseThrow(() -> new RuntimeException("User Story not found with id : "+id_us));
-        productBackLog.getUserStories().remove(id_us);
-    }
 }
