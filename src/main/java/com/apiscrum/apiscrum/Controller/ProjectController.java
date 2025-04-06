f3370ad34e6d364f4b12bd5adf79808f941fdb31
Hasnae Amr <150762808+HasnaeAmr@users.noreply.github.com>
2025-04-06 10:22:01 +0100

    Update Project Controller

--- src/main/java/com/apiscrum/apiscrum/Controller/ProjectController.java
+++ src/main/java/com/apiscrum/apiscrum/Controller/ProjectController.java
@@ -1,28 +1,68 @@
-package com.apiscrum.apiscrum.Controller;
+package com.apiscrum.APIScrum.Controller;
 
-
-import com.apiscrum.apiscrum.Entity.Project;
-import com.apiscrum.apiscrum.Service.ProjectService;
+import com.apiscrum.APIScrum.DTO.ProjectDto;
+import com.apiscrum.APIScrum.Entity.Project;
+import com.apiscrum.APIScrum.Service.ProjectService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("*")
-@AllArgsConstructor
 @RestController
 @RequestMapping("/api/project")
 public class ProjectController {
-    private  ProjectService projectService;
+
+    @Autowired
+    private ProjectService projectService;
 
     @PostMapping
-    public void createProject(@RequestBody Project project) {
-        projectService.addProject(project);
+    public ResponseEntity<?> createProject(@Valid @RequestBody ProjectDto project) {
+        try {
+            ProjectDto createdProject = projectService.addProject(project);
+            return new ResponseEntity<>("Project registered successfully with ID: " + createdProject.getId(), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProject(@PathVariable Long id, @Valid @RequestBody ProjectDto project) {
+        try{
+           return ResponseEntity.ok(projectService.updateProject(project, id));
+        }catch(Exception e){
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Project> getProject(@PathVariable("id") Long id) {
-        Project project = projectService.getProject(id);
-        return ResponseEntity.ok(project);
+    public ResponseEntity<?> getProject(@PathVariable("id") Long id) {
+        try {
+            ProjectDto project = projectService.getProject(id);
+            return new ResponseEntity<>(project, HttpStatus.OK);
+        }catch(Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+    @GetMapping("/{id}/")
+    public ResponseEntity<?> getProjectByProductBackLog(@PathVariable("id") Long id){
+        try {
+            return getProjectByProductBackLog(id);
+        }catch(Exception e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProject(@PathVariable("id") Long id) {
+        try {
+            projectService.deleteProject(id);
+            return ResponseEntity.ok("Project deleted successfully");
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error: " + ex.getMessage());
+        }
+    }
 }
